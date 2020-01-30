0e8d8cc8b1ba8d5fcffb1abbe688041b27b4158c
mokhovav <andrey.v.mohov@gmail.com>
2020-01-30 21:59:13 +0300

    exchanged dockerimage.yml

--- .github/scripts/CRT.sh
+++ .github/scripts/CRT.sh
@@ -1,3 +1,6 @@
-sudo mkdir /etc/docker/certs.d/78.47.167.60:5000
-sudo cp ./.github/scripts/domain.key /etc/docker/certs.d/78.47.167.60:5000/domain.key
-sudo cp ./.github/scripts/domain.crt /etc/docker/certs.d/78.47.167.60:5000/domain.cert
+docker --config ./.github/domain.json
+
+
+#sudo mkdir /etc/docker/certs.d/78.47.167.60:5000
+#sudo cp ./.github/scripts/domain.key /etc/docker/certs.d/78.47.167.60:5000/domain.key
+#sudo cp ./.github/scripts/domain.crt /etc/docker/certs.d/78.47.167.60:5000/domain.cert
